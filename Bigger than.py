17bdc48e21cab915b2f579b6060facfec160498b
Aiden B <133374942+VeganMessiah@users.noreply.github.com>
2023-05-12 15:37:39 +0100

    Add files via upload

---
+++ Bigger than.py
@@ -0,0 +1,15 @@
+fNum = int(input("Please enter a number:"))
+print(fNum)
+sNum = int(input("Please enter a LARGER number:"))
+print(sNum)
+remainNum = sNum-fNum
+pNum = remainNum/fNum*100
+print(sNum,"is",remainNum,"larger than",fNum)
+print("Also,",sNum,"is",pNum,"percent larger then",fNum)
+
+
+
+
+
+               
+
